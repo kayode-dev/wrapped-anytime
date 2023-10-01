60cbd7befeb58363eacd9be0466555307732cabc
Kayode <babalolakayode44@gmail.com>
2023-10-01 08:33:29 +0100

    viewartist showing

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getReturnedParamsFromAuth, getTopArtist } from "../functions/auth";
 import TopArtist from "./topArtist";
 const Review = () => {
-  const [topArtists, setTopArtist] = useState({});
+  const [topArtists, setTopArtist] = useState([]);
   useEffect(() => {
     if (window.location.hash) {
       const { access_token } = getReturnedParamsFromAuth(window.location.hash);
@@ -12,14 +12,24 @@ const Review = () => {
       };
       setData();
     }
-  },[]);
+  }, []);
 
   console.log(topArtists);
 
   return (
-    <div className="text-white">
-      <p>Your top Artist's</p>
-      <ul></ul>
+    <div className="flex flex-col justify-around items-center text-white p-8 gap-10">
+      <p className="text-lg font-bold md:text-3xl">Your top Artist's</p>
+      <ul className="flex flex-col md:flex-row gap-4 w-4/5">
+        {topArtists.map((artist) => {
+          return (
+            <TopArtist
+              name={artist.name}
+              image={artist.images[0].url}
+              standing={topArtists.lastIndexOf(artist) + 1}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- src/components/topArtist.jsx
+++ src/components/topArtist.jsx
@@ -1,9 +1,16 @@
 const TopArtist = ({ image, name, standing }) => {
   return (
-    <li>
-      <p>{standing}</p>
-      <img src={image} alt="" />
-      <p>{name}</p>
+    <li className="flex flex-col">
+      <div className="flex flex-row md:flex-col gap-4 items-center justify-between">
+        <div className="flex items-center gap-4">
+          <p className="font-bold">{standing}</p>
+          <div>
+            <img src={image} alt="" className="h-16 w-16 md:h-20 md:w-20" />
+          </div>
+        </div>
+        <p className="font-bold">{name}</p>
+      </div>
+      <hr className="md:hidden" />
     </li>
   );
 };
